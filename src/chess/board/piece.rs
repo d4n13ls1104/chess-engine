9e98dfc6253241726adff7aecba82247c9fe97b5
d4n13ls1104 <d4n13lsofficial@gmail.com>
2023-03-18 14:02:07 -0400

    refactor Piece::to_char, remove Piece::get_value

--- src/chess/board/piece.rs
+++ src/chess/board/piece.rs
@@ -21,60 +21,25 @@ pub struct Piece {
 }
 
 impl Piece {
-    pub fn get_value(&self) -> i32 {
-        if self.color == Color::White {
-            match self.piece_kind {
-                PieceKind::Pawn => 1,
-                PieceKind::Knight => 3,
-                PieceKind::Bishop => 3,
-                PieceKind::Rook => 5,
-                PieceKind::Queen => 9,
-                PieceKind::King => 10_000_000
-            }
-        } else {
-            match self.piece_kind {
-                PieceKind::Pawn => -1,
-                PieceKind::Knight => -3,
-                PieceKind::Bishop => -3,
-                PieceKind::Rook => -5,
-                PieceKind::Queen => -9,
-                PieceKind::King => -10_000_000
-            }
-        }
-    }
-    pub fn get_direction_offsets(&self) -> Vec<(isize, isize)> {
-         match self.piece_kind {
-            PieceKind::Pawn => vec![(1, 0), (-1, 0), (2, 0), (-2, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)],
-            PieceKind::Knight => vec![(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)],
-            PieceKind::Bishop => vec![(1, 1), (1, -1), (-1, 1), (-1, -1)],
-            PieceKind::Rook => vec![(0, 1), (0, -1), (1, 0), (-1, 0)],
-            PieceKind::Queen => vec![(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)],
-            PieceKind::King => vec![(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)],
-        }
-    }
-
     pub fn to_char(self) -> char {
-        match (self.piece_kind, self.color) {
-            (PieceKind::Pawn, Color::White) => 'P',
-            (PieceKind::Knight, Color::White) => 'N',
-            (PieceKind::Bishop, Color::White) => 'B',
-            (PieceKind::Rook, Color::White) => 'R',
-            (PieceKind::Queen, Color::White) => 'Q',
-            (PieceKind::King, Color::White) => 'K',
+        let character = match self.piece_kind {
+            PieceKind::Pawn => 'P',
+            PieceKind::Knight => 'N',
+            PieceKind::Bishop => 'B',
+            PieceKind::Rook => 'R',
+            PieceKind::Queen => 'Q',
+            PieceKind::King => 'K',
+        };
 
-            (PieceKind::Pawn, Color::Black) => 'p',
-            (PieceKind::Knight, Color::Black) => 'n',
-            (PieceKind::Bishop, Color::Black) => 'b',
-            (PieceKind::Rook, Color::Black) => 'r',
-            (PieceKind::Queen, Color::Black) => 'q',
-            (PieceKind::King, Color::Black) => 'k',
-        }
+        if self.color == Color::White {
+            character
+        } else { character.to_ascii_lowercase() }
     }
 
     pub fn from_char(c: char) -> Self {
         let color = if c.is_uppercase() { Color::White } else { Color::Black };
 
-        match c.to_uppercase().next().unwrap() {
+        match c.to_ascii_uppercase() {
             'P' => Piece {
                 piece_kind: PieceKind::Pawn,
                 color
@@ -100,6 +65,17 @@ impl Piece {
                 color
             },
             _ => panic!("Invalid Piece Character")
+        }
+    }
+
+    pub fn get_direction_offsets(&self) -> Vec<(isize, isize)> {
+        match self.piece_kind {
+            PieceKind::Pawn => vec![(1, 0), (-1, 0), (2, 0), (-2, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)],
+            PieceKind::Knight => vec![(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)],
+            PieceKind::Bishop => vec![(1, 1), (1, -1), (-1, 1), (-1, -1)],
+            PieceKind::Rook => vec![(0, 1), (0, -1), (1, 0), (-1, 0)],
+            PieceKind::Queen => vec![(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)],
+            PieceKind::King => vec![(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)],
         }
     }
 }
