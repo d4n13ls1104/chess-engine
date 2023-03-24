9c59adbbb85010ab10cf7348544addd70a3650d3
d4n13ls1104 <d4n13lsofficial@gmail.com>
2023-03-24 09:12:16 -0400

    Add code example to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod chess;
-// use chess::board::Board;
-
-// const _TEST_FEN: &str = "4k3/pppppppp/8/8/8/8/PPPPPPPP/4K3 w - - 0 1";
-// const _ITALIAN: &str = "r1bqk2r/pppp1ppp/2n2n2/2b1p3/2B1P3/2N2N2/PPPP1PPP/R1BQK2R b KQkq - 6 5";
-// const _EN_PASSANT_TEST_FEN: &str = "rnbqkbnr/ppp2ppp/4p3/3pP3/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3";
+use chess::board::Board;
 
 fn main() {
+    let mut board = match Board::from_fen("start") {
+        Ok(b) => b,
+        Err(err) => panic!("Error initializing board: {err}")
+    };
 
+    let m = board.get_legal_moves()[0];
+    board.move_piece(m).expect("Illegal move");
+
+    println!("{}", board.to_fen());
 }
