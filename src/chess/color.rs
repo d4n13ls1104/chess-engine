50e5ccca73fc045dd58f7a63ae3e78f5f80f1379
d4n13ls1104 <d4n13lsofficial@gmail.com>
2023-06-01 04:59:29 -0400

    add Color enum for active_turn

---
+++ src/chess/color.rs
@@ -0,0 +1,20 @@
+pub enum Color {
+    White,
+    Black,
+}
+
+impl Color {
+    pub fn invert(&self) -> Self {
+        match self {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
+        }
+    }
+    pub fn from_char(c: char) -> Self {
+        match c.to_ascii_lowercase() {
+            'w' => Color::White,
+            'b' => Color::Black,
+            _ => panic!("Invalid color char"),
+        }
+    }
+}
