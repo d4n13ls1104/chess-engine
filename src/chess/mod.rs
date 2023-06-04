191273ef99f7c03c505375c59ebe46c5063ba9d7
d4n13ls1104 <d4n13lsofficial@gmail.com>
2023-06-04 08:38:45 -0400

    make Chess wrapper struct

--- src/chess/mod.rs
+++ src/chess/mod.rs
@@ -1,5 +1,14 @@
+#![allow(unused)]
+#![allow(dead_code)]
+use self::position::Position;
+
 mod bitboard;
 mod board;
 mod castle_flags;
 mod color;
 mod position;
+
+#[derive(Default)]
+pub struct Chess {
+    pub position: Position,
+}
